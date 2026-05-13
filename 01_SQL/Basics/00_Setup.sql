87e5c23ecc2a2ff4ca6e55b5d07f45023e59708d
Akul Rajput <akulrajpoot24@gmail.com>
2026-05-13 18:52:58 +0530

    Adding more data in table

--- 01_SQL/Basics/00_Setup.sql
+++ 01_SQL/Basics/00_Setup.sql
@@ -7,7 +7,7 @@ CREATE TABLE employees (
     emp_id INT PRIMARY KEY,
     name VARCHAR(50),
     age INT,
-    salary INT,
+    salary DECIMAL(10,2),
     department VARCHAR(50),
     city VARCHAR(50)
 );
@@ -21,7 +21,14 @@ INSERT INTO employees VALUES
 (5, 'Karan', 27, 45000, 'Finance', 'Delhi'),
 (6, 'Sneha', 29, 72000, 'HR', 'Mumbai'),
 (7, 'Vikas', 35, 90000, 'IT', 'Pune'),
-(8, 'Anjali', 24, 40000, 'HR', 'Delhi');
+(8, 'Anjali', 24, 40000, 'HR', 'Delhi'),
+(9, 'Amit', 32, 70000, 'IT', 'Mumbai'),
+(10, 'Rohit', 31, NULL, 'IT', 'Delhi'),
+(11, 'Simran', 27, 55555.75, 'HR', NULL),
+(12, 'Arjun', 29, NULL, 'Finance', 'Mumbai'),
+(13, 'Kavya', 26, 48234.50, 'IT', NULL),
+(14, 'Manish', 33, 91000.25, 'Finance', 'Delhi')
+;
 
 #creating second table 
 CREATE TABLE departments (
@@ -35,7 +42,4 @@ INSERT INTO departments VALUES
 ('Finance', 'Ramesh'),
 ('HR', 'Anita');
 
-
-INSERT INTO employees VALUES
-(9, 'Amit', 32, 70000, 'IT', 'Mumbai');
 
