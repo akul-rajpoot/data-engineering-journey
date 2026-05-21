e1fac85dca2162a2563332130bbb377380fd71ed
Akul Rajput <akulrajpoot24@gmail.com>
2026-05-21 17:31:11 +0530

    Adding table in file

--- 01_SQL/Case_Studies/03_case_study_tables.sql
+++ 01_SQL/Case_Studies/03_case_study_tables.sql
@@ -45,3 +45,51 @@ INSERT INTO payments03 VALUES
 (6, 106, 'Cash'),
 (7, 107, 'Card'),
 (8, 108, 'UPI');
+
+
+ ####################
+ #🚀 🧠 CASE STUDY 2: Subscription & Revenue Analytics
+ ###############
+ 
+ CREATE TABLE user_details03 (
+    user_id INT,
+    user_name VARCHAR(50),
+    signup_date DATE
+);
+
+INSERT INTO user_details03 VALUES
+(1, 'Amit', '2023-01-01'),
+(2, 'Rahul', '2023-01-02'),
+(3, 'Neha', '2023-01-03'),
+(4, 'Priya', '2023-01-04');
+
+CREATE TABLE subscriptions03 (
+    sub_id INT,
+    user_id INT,
+    start_date DATE,
+    end_date DATE,
+    plan VARCHAR(20)
+);
+
+INSERT INTO subscriptions03 VALUES
+(101, 1, '2023-01-01', '2023-01-10', 'Basic'),
+(102, 1, '2023-01-11', '2023-01-20', 'Premium'),
+(103, 2, '2023-01-02', '2023-01-15', 'Basic'),
+(104, 3, '2023-01-03', '2023-01-08', 'Basic'),
+(105, 3, '2023-01-10', '2023-01-20', 'Premium'),
+(106, 4, '2023-01-04', '2023-01-25', 'Premium');
+
+CREATE TABLE payments_details03 (
+    payment_id INT,
+    user_id INT,
+    payment_date DATE,
+    amount INT
+);
+
+INSERT INTO payments_details03 VALUES
+(1, 1, '2023-01-01', 100),
+(2, 1, '2023-01-11', 200),
+(3, 2, '2023-01-02', 100),
+(4, 3, '2023-01-03', 100),
+(5, 3, '2023-01-10', 200),
+(6, 4, '2023-01-04', 200);
